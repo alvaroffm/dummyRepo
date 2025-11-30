4bd3fed3d88971d6b0ceecef757149ceeadbab49
alvaroffm <alvaroffm@gmail.com>
2025-11-30 12:04:18 +0100

    feat: Update application version to 0.2 and refine build directory cleanup to target the versioned output folder.

--- setup.py
+++ setup.py
@@ -3,29 +3,25 @@ import shutil
 
 from cx_Freeze import Executable, setup
 
-# --- BLOQUE DE LIMPIEZA ---
-if os.path.exists("build"):
+
+VERSION = "0.2"
+nombre_carpeta_salida = f"build/v{VERSION}_ControlConfigurationApp"
+
+if os.path.exists(nombre_carpeta_salida):
     print("🧹 Eliminando carpeta 'build' antigua...")
     try:
-        shutil.rmtree("build")
+        shutil.rmtree(nombre_carpeta_salida)
         print("✅ Carpeta 'build' eliminada.")
     except Exception as e:
         print(f"⚠️ No se pudo eliminar la carpeta 'build': {e}")
-# --------------------------
-# 1. DEFINIMOS LA VERSIÓN AQUÍ
-VERSION = "0.1"
-nombre_carpeta_salida = f"build/v{VERSION}_ControlConfigurationApp"
+
 print(f"Configurando build para v{VERSION}...")
-# 2. OPCIONES DE CONSTRUCCIÓN
 build_exe_options = {
     "packages": ["streamlit", "pandas", "numpy", "altair"],
-    # COPIAMOS LA CARPETA SRC COMPLETA
-    # Origen: "src" (la carpeta real en tu disco)
-    # Destino: "dashboard/src" (la carpeta dentro del exe)
     "include_files": [("src", "src")],
     "build_exe": nombre_carpeta_salida,
 }
-# 3. CONFIGURACIÓN FINAL
+
 setup(
     name="ControlConfigurationApp",
     version=VERSION,
